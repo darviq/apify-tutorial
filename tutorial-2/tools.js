bf54c30a31eb8541579cf73d6192c2d33290107f
Dmytro Chernovskyi <dimcher911@gmail.com>
2021-06-23 09:07:20 +0300

    Delete comments

--- tutorial-2/tools.js
+++ tutorial-2/tools.js
@@ -8,11 +8,3 @@ exports.createRouter = (globalContext) => {
         return route(requestContext, globalContext, statistics);
     };
 };
-
-// exports.createRouter = (globalContext) => {
-//     return async function (routeName, requestContext) {
-//         const route = routes[routeName];
-//         if (!route) throw new Error(`No route for name: ${routeName}`);
-//         return route(requestContext, globalContext);
-//     };
-// };
